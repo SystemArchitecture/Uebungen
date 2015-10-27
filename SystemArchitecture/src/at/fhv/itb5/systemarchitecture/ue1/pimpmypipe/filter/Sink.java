7cfe96e6db277b658839de5ac4549561c690fea9
U-ANS\simon_000 <simon.angerer@hotmail.com>
2015-10-27 14:55:12 +0100

    inital commit

---
+++ SystemArchitecture/src/at/fhv/itb5/systemarchitecture/ue1/pimpmypipe/filter/Sink.java
@@ -0,0 +1,12 @@
+package at.fhv.itb5.systemarchitecture.ue1.pimpmypipe.filter;
+
+import at.fhv.itb5.systemarchitecture.ue1.pimpmypipe.interfaces.Readable;
+
+public abstract class Sink<T> implements Readable<T>, Runnable{
+
+    public void run() {
+            
+    }
+    
+    
+}
---
+++ SystemArchitecture/src/at/fhv/itb5/systemarchitecture/ue1/pimpmypipe/insys/FileSource.java
@@ -0,0 +1,5 @@
+package at.fhv.itb5.systemarchitecture.ue1.pimpmypipe.insys;
+
+public class FileSource {
+
+}
