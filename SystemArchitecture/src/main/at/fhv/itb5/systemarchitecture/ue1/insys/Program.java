39b9f9cb894bea397b04fb5d107b7270a0090f95
Norskan <simon.angerer@hotmail.com>
2015-11-02 14:14:49 +0100

    fixed resource directory danke herr griesser
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

--- SystemArchitecture/src/main/at/fhv/itb5/systemarchitecture/ue1/insys/Program.java
+++ SystemArchitecture/src/main/at/fhv/itb5/systemarchitecture/ue1/insys/Program.java
@@ -1,12 +1,13 @@
 package main.at.fhv.itb5.systemarchitecture.ue1.insys;
 
 import main.at.fhv.itb5.systemarchitecture.ue1.insys.sink.FileSourceActive;
+import main.at.fhv.itb5.systemarchitecture.ue1.insys.source.ConsoleSinkPassive;
 import main.at.fhv.itb5.systemarchitecture.ue1.pimpmypipe.filter.source.SourceActive;
 
 public class Program {
 
 	public static void main(String[] args) {
-		SourceActive<String> source = new FileSourceActive(null);
+		SourceActive<String> source = new FileSourceActive(new ConsoleSinkPassive());
 		source.run();
 	}
 
