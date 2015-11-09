53f67e83a72afdbad883bbb65fde2c76b2f74e6e
Norskan <simon.angerer@hotmail.com>
2015-11-09 12:49:49 +0100

    ue2 initial commit
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

--- SystemArchitecture/src/main/at/fhv/itb5/systemarchitecture/ue1/indsys/Program.java
+++ SystemArchitecture/src/main/at/fhv/itb5/systemarchitecture/ue1/indsys/Program.java
@@ -33,8 +33,8 @@ public class Program {
 	private static Application _application;
 
 	/*
-	 * -> A Pull/Push sinkFileIndexPath -> B Pull/Push sinkFileIndexPath
-	 * sinkFilePath alignment lineSize
+	 * -> A Pull/Push sinkFileIndexPath 
+	 * -> B Pull/Push sinkFileIndexPath sinkFilePath alignment lineSize
 	 */
 	public static void main(String[] args) {
 		if (!isInDebugMode) {
