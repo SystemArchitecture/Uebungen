2b20248e1fd84ba1019a27d070427ef5172c8192
Norskan <simon.angerer@hotmail.com>
2015-11-23 17:50:24 +0100

    some stuff
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

--- SystemArchitecture/src/main/at/fhv/itb5/systemarchitecture/ue3/FileLoader.java
+++ SystemArchitecture/src/main/at/fhv/itb5/systemarchitecture/ue3/FileLoader.java
@@ -1,15 +1,19 @@
 package main.at.fhv.itb5.systemarchitecture.ue3;
 
-public class FileLoaderComponent {
+public class FileLoader {
 	
 	private String _path;
+	
+	public FileLoader() {
+		_path = "";
+	}
 	
 	public void setImagePath(String value) {
 		_path = value;
 		System.out.println(value);
 	}
 	
-	public String getImagePaht() {
-		
+	public String getImagePath() {
+		return _path;
 	}
 }
